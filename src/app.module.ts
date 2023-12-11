7deb31d498eb275768ab861c8c927326b8284df8
Vinicius Martins <viniciuslemos179@gmail.com>
2023-12-11 01:53:27 -0300

    config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST_DB'),
+        port: parseInt(configService.get('PORT_DB')),
+        username: configService.get('USER_DB'),
+        password: configService.get('PASS_DB'),
+        database: configService.get('NAME_DB'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        migrations: ['dist/database/migrations/*.{js, ts}'],
+        cli: {
+          migrationsDir: 'src/database/migrations',
+        },
+        synchronize: false,
+        logging: configService.get('NODE_ENV') === 'local' ? true : false,
+      }),
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
